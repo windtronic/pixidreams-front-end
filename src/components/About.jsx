ead2a9d1f920259a79ee64f41a0384a553c498af
Jason Bundy <jason.bundy@me.com>
2023-03-18 13:08:48 -0400

    update api.jsxx and About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import Client from "../services/api";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <section>
+        <div>ABOUT</div>
+      </section>
+      <section>
+        <div>LATEST</div>
+      </section>
+    </div>
+  );
 };
 
 export default About;
