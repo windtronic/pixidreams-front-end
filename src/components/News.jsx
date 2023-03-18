9c73facc4daddf218971d7d988ac5b4f41956974
Jason Bundy <jason.bundy@me.com>
2023-03-18 15:25:14 -0400

    setup preliminary route setup news feed from TMDB

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,16 +1,43 @@
-import { useState } from "react";
-import { BASE_URL } from "../globals";
+import { useState, useEffect } from "react";
+import { BASE_URL, POSTER_URL } from "../globals";
+import Client from "../services/api";
 
 const News = () => {
-  const [newsFeed, setNewsFeed] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [respPages, setRespPages] = useState(0);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const response = await Client.get(
+        `${BASE_URL}api_key=${process.env.REACT_APP_TMDB_KEY}`
+      );
+      //console.log(response.data.results);
+      //console.log(response.data.total_pages);
+      setMovies(response.data.results);
+      setRespPages(response.data.total_pages);
+    };
+    getMovies();
+  }, []);
 
   return (
     <div>
       <section>
-        <div>News</div>
+        <div>NEWS</div>
       </section>
       <section>
-        <div>Data goes here...</div>
+        <div>
+          {movies.map((movie, index) => {
+            return (
+              <div key={index}>
+                <section>{movie.title}</section>
+                <section>
+                  <img src={POSTER_URL + movie.poster_path} alt="poster" />
+                </section>
+                <section>{movie.overview}</section>
+              </div>
+            );
+          })}
+        </div>
       </section>
     </div>
   );
--- src/globals.jsx
+++ src/globals.jsx
@@ -1,1 +1,2 @@
 export const BASE_URL = "https://api.themoviedb.org/3/trending/movie/week?";
+export const POSTER_URL = "https://image.tmdb.org/t/p/original/";
