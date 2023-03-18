436952fe9dcc765c7cecb9d216d73b6c0fe65c1a
Jason Bundy <jason.bundy@me.com>
2023-03-18 13:58:37 -0400

    updating components

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import { BASE_URL } from "../globals";
+
 const News = () => {
-  return <div>News</div>;
+  const [newsFeed, setNewsFeed] = useState([]);
+
+  return (
+    <div>
+      <section>
+        <div>News</div>
+      </section>
+      <section>
+        <div>Data goes here...</div>
+      </section>
+    </div>
+  );
 };
 
 export default News;
