38b54a1df65ea0e532d3f6767a6771ee506aa7c3
Jason Bundy <jason.bundy@me.com>
2023-03-19 15:14:42 -0400

    added basic code for connecting to the back-end

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 import Client from "../services/api";
 
 const Movies = () => {
+  const [getContent, setContent] = useState([]);
+
+  useEffect(() => {
+    const getContent = async (req, res) => {
+      const content = await Client.get("/");
+    };
+  });
+
   return <div>Movies</div>;
 };
 
