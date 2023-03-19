9afb29f2e701b5ed49b3005f0ed348a151d06fc9
Jason Bundy <jason.bundy@me.com>
2023-03-19 11:33:45 -0400

    updated routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,18 @@
-const Nav = () => {
-  return <div>Navigation goes here...</div>;
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>PixiDreams</h1>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/movies">Movies</Link>
+        <Link to="/news">News</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </nav>
+  );
 };
 
-export default Nav;
+export default Navbar;
