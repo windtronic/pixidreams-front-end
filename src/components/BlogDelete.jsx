563bb850a546d3ae10cd9f5da9bd1ea414cae970
Jason Bundy <jason.bundy@me.com>
2023-03-20 16:25:22 -0400

    updating blogdelete

--- src/components/BlogDelete.jsx
+++ src/components/BlogDelete.jsx
@@ -1,9 +1,8 @@
-import Client from '../services/api'
+// import Client from '../services/api'
 
-const BlogDelete = (id) => {
-  Client.delete(`/api/posts/${id}`)
-  console.log(id)
-}
+// const BlogDelete = (id) => {
+//   Client.delete(`/api/posts/${id}`)
+//   console.log(id)
+// }
 
-export default BlogDelete;
-
+// export default BlogDelete;
