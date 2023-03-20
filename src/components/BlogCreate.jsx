d7c2dd47c3fbd9ad1a76a15b0b1afa83e25a2631
Toni <windtronic89@gmail.com>
2023-03-20 09:52:33 -0600

    working on create

--- src/components/BlogCreate.jsx
+++ src/components/BlogCreate.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import Client from "../services/api";
+
+
 const BlogCreate = () => {
+const [createBlog, setCreateBlog] = useState([])
+
+useEffect(() => {
+  const createBlogPost = async (req, res) => {
+    const response = await Client.post(`/api/posts`);
+    console.log(response.data);
+    setCreateBlog(response.data);
+  };
+  createBlogPost();
+}, []);
+
+
+
+
+
   return <div></div>;
 };
 
 export default BlogCreate;
+
+
+
