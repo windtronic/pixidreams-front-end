b5c71cb2a97dbfa310148e92029eb9a929a9324b
Toni <windtronic89@gmail.com>
2023-03-23 09:46:16 -0600

    likes Work!

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,21 +1,19 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Client from "../services/api";
-
 const MovieDetails = (props) => {
   const { id } = useParams();
   const [movie, setMovie] = useState([]);
   const [singleComment, setSingleComment] = useState([]);
   const [createComment, setCreateComment] = useState([]);
-  const [likes, setLikes] = useState(0);
-
+  const [likes, setLikes] = useState([]);
+  const [reviewLike, setReviewLike] = useState(0)
   const [formData, setFormData] = useState({
     comment: "",
     likes: 0,
     userId: props.movieContent.userId,
     contentId: id,
   });
-
   useEffect(() => {
     const getSelectedMovie = async () => {
       if (props.movieContent && props.movieContent[id]) {
@@ -28,16 +26,44 @@ const MovieDetails = (props) => {
     };
     getSelectedMovie();
   }, [props.movieContent]);
-
   const handleChange = (event) => {
     event.preventDefault();
     setFormData({ ...formData, [event.target.id]: event.target.value });
   };
-
-  function handleLikeClick() {
-    setLikes((prevLikes) => prevLikes + 1);
+  // function handleLikeClick() {
+  //   let like = likes + 1
+  //   setLikes(like)
+  // }
+  useEffect(() => {
+    const storedLikes = localStorage.getItem("likes");
+    if (storedLikes) {
+      setLikes(JSON.parse(storedLikes));
+    }
+  }, []);
+  function handleLikeClick(commentId) {
+    setLikes((prevLikes) => {
+      const likesCopy = [...prevLikes];
+      const commentLikesIndex = likesCopy.findIndex((like) => like.commentId === commentId);
+      if (commentLikesIndex === -1) {
+        likesCopy.push({ commentId, count: 1 });
+      } else {
+        likesCopy[commentLikesIndex].count++;
+      }
+      localStorage.setItem("likes", JSON.stringify(likesCopy));
+      return likesCopy;
+    });
   }
-
+  useEffect(() => {
+    const storedLike = localStorage.getItem('reviewLike');
+    if (storedLike) {
+      setReviewLike(parseInt(storedLike));
+    }
+  }, []);
+  function handleReviewLike() {
+    let like = reviewLike + 1
+    setReviewLike(like)
+    localStorage.setItem('reviewLike', like);
+  }
   const handleSubmit = async (event) => {
     event.preventDefault();
     const response = await Client.post(`/api/comments/${id}`, formData);
@@ -46,17 +72,14 @@ const MovieDetails = (props) => {
     setFormData({ ...formData, comment: "" });
     console.log("Submitted comment:", singleComment);
   };
-
   const displayComments = async () => {
     const response = await Client.get(`/api/comments/view/${id}`);
     setSingleComment(response.data);
     setCreateComment(response.data);
   };
-
   useEffect(() => {
     displayComments();
   }, [id]);
-
   return (
     <div>
       <div className="pageContainer">
@@ -67,8 +90,8 @@ const MovieDetails = (props) => {
               <span>Review: {movie.review}</span>
             </div>
             <div>
-              <span>Likes: {movie.likes}</span>
-              <button onClick={() => handleLikeClick()}>Like</button>
+              <span>Likes: {reviewLike}</span>
+              <button onClick={() => handleReviewLike()}>Like</button>
             </div>
             <div>
               <form onSubmit={handleSubmit}>
@@ -84,19 +107,22 @@ const MovieDetails = (props) => {
               </form>
             </div>
             <div>
-              {singleComment &&
-                Array.isArray(singleComment) &&
-                singleComment.map((comment) => (
+            {singleComment &&
+              Array.isArray(singleComment) &&
+              singleComment.map((comment) => {
+                const commentLikes = likes.find((like) => like.commentId === comment.id);
+                return (
                   <div key={comment.id}>
                     <div>
                       <span>Comments: {comment.comment}</span>
                     </div>
                     <div>
-                      <span>Likes: {movie.likes}</span>
-                      <button onClick={() => handleLikeClick()}>Like</button>
+                      <span>Likes: {commentLikes ? commentLikes.count : 0}</span>
+                      <button onClick={() => handleLikeClick(comment.id)}>Like</button>
                     </div>
                   </div>
-                ))}
+                );
+              })}
             </div>
             {createComment && (
               <div>
@@ -109,5 +135,4 @@ const MovieDetails = (props) => {
     </div>
   );
 };
-
 export default MovieDetails;
